74f2798defdd47ff7713df4ebcf772c3a498d02d
Frank James <frank.a.james@gmail.com>
2013-05-08 20:01:50 +0100

    adding lu decomp and correct det

--- src/matrices.lisp
+++ src/matrices.lisp
@@ -270,7 +270,7 @@ return (list NIL NIL)"
 	  (setf (aref mat i i) 1))
 	mat))
 
-(defun invert-mat (matrix)
+(defun invert (matrix)
   "Invert a matrix of numbers using gauss jordan elimination"
   (unless (= (array-dimension matrix 0) (array-dimension matrix 1))
 	(error "Not a square matrix."))
@@ -336,42 +336,6 @@ return (list NIL NIL)"
   "Convert a list to a vector."
   (make-array (length list) :initial-contents list))
 
-(defun sub-mat (terms i j)
-  (loop with r = 0
-	 for row in terms nconc
-	   (prog1
-		   (unless (= r j)
-			 (list 
-			  (loop with c = 0
-				 for x in row nconc
-				   (prog1
-					   (unless (= c i)
-						 (list x))
-					 (incf c)))))
-		 (incf r))))
-
-(defun det (mat)
-  "Compute the determinant of a matrix of numbers"
-  (labels ((sub-det (terms)
-			 (let ((n (length terms)))
-			   (cond
-				 ((= n 1)
-				  (car terms))
-				 ((= n 2)
-				  (destructuring-bind ((x11 x12) (x21 x22)) terms
-					(- (* x11 x22) (* x12 x21))))
-				 (t
-				  (loop with s = 1
-					 with row = (car terms)
-					 for i below n sum
-					   (let ((d (* s (car row) (sub-det (sub-mat terms i 0)))))
-						 (setf s (- s)
-							   row (cdr row))
-						 d)))))))
-	(sub-det (if (arrayp mat)
-				 (mat-list mat)
-				 mat))))
-
 (defun fpoly-det (mat)
   "Compute the determinant of a matrix of polys."
   (labels ((sub-det (terms)
@@ -396,16 +360,93 @@ return (list NIL NIL)"
 				 (mat-list mat)
 				 mat))))
 
+;;; --------------------------------------------------------
 
-(defun det-pattern (n)
-  "Return a polynomial for the determinant pattern of an n x n matrix."
-  (let ((m (make-array (list n n)
-					   :initial-contents
-					   (loop for i below n collect
-							(loop for j below n collect
-								 (make-fpoly (intern (mkstr "X" i j))
-											 1
-											 '(0 1)))))))
-	(fpoly-det m)))
+;;; lu decomposition and determinant follows
+;;; taken from  http://rosettacode.org/wiki/LU_decomposition#Common_Lisp
+;;;
+ 
+(defun mmul (A B)
+  (let* ((m (car (array-dimensions A)))
+		 (n (cadr (array-dimensions A)))
+		 (l (cadr (array-dimensions B)))
+		 (C (make-array `(,m ,l) :initial-element 0)))
+	(loop for i from 0 to (- m 1) do
+		 (loop for k from 0 to (- l 1) do
+			  (setf (aref C i k)
+					(loop for j from 0 to (- n 1)
+					   sum (* (aref A i j)
+							  (aref B j k))))))
+	C))
 
+;; Creates a nxn identity matrix.
+(defun eye (n)
+  (let ((I (make-array `(,n ,n) :initial-element 0)))
+	(loop for j from 0 to (- n 1) do
+		 (setf (aref I j j) 1))
+	I))
 
+;; Swap two rows l and k of a mxn matrix A, which is a 2D array.
+(defun swap-rows (A l k)
+  (let* ((n (cadr (array-dimensions A)))
+		 (row (make-array n :initial-element 0)))
+	(loop for j from 0 to (- n 1) do
+		 (setf (aref row j) (aref A l j))
+		 (setf (aref A l j) (aref A k j))
+		 (setf (aref A k j) (aref row j)))))
+
+;; Creates the pivoting matrix for A.
+(defun pivotize (A)
+  (let* ((n (car (array-dimensions A)))
+		 (P (eye n))
+		 (s 0))
+	(loop for j from 0 to (- n 1) do
+		 (let ((max (aref A j j))
+			   (row j))
+		   (loop for i from j to (- n 1) do
+				(if (> (aref A i j) max)
+					(setq max (aref A i j)
+						  row i)))
+		   (if (not (= j row))
+			   (progn
+				 (swap-rows P j row)
+				 (incf s)))))
+
+	;; Return P.
+	(values P s)))
+
+;; Decomposes a square matrix A by PA=LU and returns L, U and P.
+(defun lu (A)
+  (let* ((n (car (array-dimensions A)))
+		 (L (make-array `(,n ,n) :initial-element 0))
+		 (U (make-array `(,n ,n) :initial-element 0)))
+	(multiple-value-bind (P s) (pivotize A)
+	  (let ((A (mmul P A)))
+
+	(loop for j from 0 to (- n 1) do
+		 (setf (aref L j j) 1)
+		 (loop for i from 0 to j do
+			  (setf (aref U i j)
+					(- (aref A i j)
+					   (loop for k from 0 to (- i 1)
+						  sum (* (aref U k j)
+								 (aref L i k))))))
+		 (loop for i from j to (- n 1) do
+			  (setf (aref L i j)
+					(/ (- (aref A i j)
+						  (loop for k from 0 to (- j 1)
+							 sum (* (aref U k j)
+									(aref L i k))))
+					   (aref U j j)))))
+
+	;; Return L, U and P.
+	(values L U P s)))))
+
+(defun det (mat)
+  (let ((n (car (array-dimensions mat))))
+	(multiple-value-bind (l u p s) (lu mat)
+	  (declare (ignore p))
+	  (let ((d (expt -1 s)))
+		(dotimes (i n)
+		  (setf d (* d (aref l i i) (aref u i i))))
+		d))))
